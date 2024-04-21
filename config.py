bfb85872c11511d70fe68dfa3eea6a4053d8a1fe
Rohit Kumar Bisht <rohitkumarbisht@PL-Rohit-MACBOOK-Q72R6G774X.local>
2024-04-21 19:55:58 +0530

    deleted config file

--- config.py
+++
@@ -1,9 +0,0 @@
-# Define your PostgreSQL database connection settings
-db_host = 'airbyte.cqqg4q5hnscs.ap-south-1.rds.amazonaws.com'
-db_port = 5432
-db_user = 'airbyte'
-db_password = 'F648d&lTHltriVidRa0R'
-db_name = 'learninganalytics'
-schema_name = 'learninganalytics'
-table_name = "dropout_data"
-config_table = "dropout_model_config"
